6abc4c62687125c1a4d344ca1b033e0069095173
kahakai <git@kahakai.me>
2024-03-14 19:50:45 +0000

    Add Store interface for database queries

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -9,21 +9,30 @@ import (
 )
 
 // Store provides all functions to execute db queries and transactions
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(
+		ctx context.Context,
+		arg TransferTxParams,
+	) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
 	*Queries
 	db *pgxpool.Pool
 }
 
 // NewStore creates a new Store
-func NewStore(db *pgxpool.Pool) *Store {
-	return &Store{
+func NewStore(db *pgxpool.Pool) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, pgx.TxOptions{})
 	if err != nil {
 		return err
@@ -59,7 +68,7 @@ type TransferTxResult struct {
 
 // TransferTx performs a money transfer from one account the other.
 // It creates a transfer record, add adds account entries, and updates accounts' balance within a single database transaction.
-func (store *Store) TransferTx(
+func (store *SQLStore) TransferTx(
 	ctx context.Context,
 	arg TransferTxParams,
 ) (TransferTxResult, error) {
