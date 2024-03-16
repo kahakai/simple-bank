b7c8b428a975269dfee31ce7c7aadc4e5a855cf7
kahakai <git@kahakai.me>
2024-03-16 21:29:25 +0000

    Add test cases for POST /accounts endpoint

--- api/account_test.go
+++ api/account_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	mock_db "github.com/kahakai/simple-bank/db/mock"
 	db "github.com/kahakai/simple-bank/db/sqlc"
 	"github.com/kahakai/simple-bank/util"
@@ -23,6 +24,118 @@ func randomAccount() db.Account {
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
+	}
+}
+
+func TestCreateAccountAPI(t *testing.T) {
+	req := createAccountRequest{
+		Owner:    util.RandomOwner(),
+		Currency: util.RandomCurrency(),
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account := db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    arg.Owner,
+		Currency: arg.Currency,
+		Balance:  arg.Balance,
+	}
+
+	testCases := []struct {
+		name           string
+		accountRequest createAccountRequest
+		buildStubs     func(store *mock_db.MockStore)
+		checkResponse  func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:           "OK",
+			accountRequest: req,
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, account)
+			},
+		},
+		{
+			name:           "Forbidden (foreign_key_violation)",
+			accountRequest: req,
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(db.Account{}, &pgconn.PgError{
+						Code: "23503",
+					})
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusForbidden, recorder.Code)
+			},
+		},
+		{
+			name:           "Forbidden (unique_violation)",
+			accountRequest: req,
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(db.Account{}, &pgconn.PgError{
+						Code: "23505",
+					})
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusForbidden, recorder.Code)
+			},
+		},
+		{
+			name:           "InternalError",
+			accountRequest: req,
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(db.Account{}, pgx.ErrTxClosed)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock_db.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			body := tc.accountRequest
+			jsonBody, err := json.Marshal(body)
+			require.NoError(t, err)
+
+			bodyReader := bytes.NewReader(jsonBody)
+
+			request, err := http.NewRequest(http.MethodPost, "/accounts", bodyReader)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
 	}
 }
 
